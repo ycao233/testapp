b909569b4e28865f47af57bd707868feead86379
Yang Cao <yacao@microsoft.com>
2015-02-10 13:27:31 -0500

    make another change 4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,6 @@ public class App {
     //1 more change
     public static void main(String[] args) {
         Calculator c = new Calculator();
-        System.out.println(c.addition(1, 4));
     }
 
 }
