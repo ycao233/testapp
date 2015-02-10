a5477521009a92a7e017616c9a1a95ac5d0f88e7
Yang Cao <yacao@microsoft.com>
2015-02-10 12:12:40 -0500

    make another rm + add change

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,10 +4,10 @@
 public class App {
 
     public static void main(String[] args) {
-        //make a change
         //make another change
         Calculator c = new Calculator();
-        System.out.println(c.addition(1, 2));
+        System.out.println(c.addition(1, 4));
+        //make the change here
     }
 
 }
