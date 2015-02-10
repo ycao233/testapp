765f02f86b5624898ac47660a91240193e06afd9
Yang Cao <yacao@microsoft.com>
2015-02-10 13:13:55 -0500

    make another change 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,13 +2,10 @@
  * Created by yacao on 1/30/2015.
  */
 public class App {
-
+    //add something
     public static void main(String[] args) {
-        //make another change
         Calculator c = new Calculator();
         System.out.println(c.addition(1, 4));
-        //make the change here
-        //What happened to my change???
     }
 
 }
